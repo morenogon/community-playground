6e1860ed4ff259d2945807db11e6392cc10a8384
Alba Moreno <alba.moreno95@gmail.com>
2022-02-22 20:22:28 +0100

    Logout feature success

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import '../styles/Button.scss';
 import { Avatar } from '@nextui-org/react';
 
-const Button = ({ type, LeftIcon, RightIcon, avatar, title }) => {
+const Button = ({ type, LeftIcon, RightIcon, avatar, title, onClick }) => {
   return (
-    <div className={type && type === 'post' ? 'button button-post' : 'button'}>
+    <div className={type && type === 'post' ? 'button button-post' : 'button'} onClick={onClick}>
       {LeftIcon && <LeftIcon className="button__left-icon" />}
       {avatar && <Avatar src={avatar} size="sm" />}
       <h3>{title ? title : 'Me'}</h3>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../features/userSlice';
+import { getAuth, signOut } from 'firebase/auth';
+
+// styles
 import '../styles/Header.scss';
+
+// components
 import Button from './Button';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+  const auth = getAuth();
+
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () => {
+    debugger;
+    dispatch(logout());
+
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        toast.success('Sign-out successful');
+      })
+      .catch((error) => {
+        // An error happened.
+        toast.warn('An error happened');
+      });
+  };
+
   return (
     <div className="header">
       <div className="header__left">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
-          alt="linkedin logo"
-        />
+        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin logo" />
       </div>
       <div className="header__right">
         <Button
           type="primary"
-          title="Name Surname"
-          RightIcon={ExpandMoreIcon}
+          title="Sign out"
           avatar="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+          onClick={logoutOfApp}
         />
       </div>
     </div>
